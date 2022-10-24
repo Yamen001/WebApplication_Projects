09c96352172afc9a9ad4514702d826b449cd069b
Kareem <kareemsafar2018@gmail.com>
2022-10-24 11:37:41 +0300

    html configuration is done
    Signed-off-by: Kareem <kareemsafar2018@gmail.com>

---
+++ .history/Doctor_webApplication/main/views_20221024111741.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def login(request(method, url, body=None, headers={}))
---
+++ .history/Doctor_webApplication/main/views_20221024111804.py
@@ -0,0 +1,11 @@
+from re import template
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def login(request):
+    template = loader.get_template()
