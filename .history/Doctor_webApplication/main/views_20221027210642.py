a7b9cec6b067b4948ed553c67b3a2918aa8a7cb9
Kareem <kareemsafar2018@gmail.com>
2022-10-27 22:39:07 +0300

    csrf issues - login issues
    Signed-off-by: Kareem <kareemsafar2018@gmail.com>

---
+++ .history/Doctor_webApplication/main/views_20221027210642.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+from .models import Hero
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def main(request):
+    hero = Hero()
+    hero.title = "Welcome Here!"
+    hero.sub_title = "We are so happy to be in your service"
+    hero.description = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum fugit est omnis ut cum vel libero accusantium necessitatibus placeat, assumenda consequuntur dicta tempore iusto et"
+    return render(request, 'main.html', {'hero': hero})
+
+
+def logins(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    options = request.POST.get('options')
+    submitbutton = request.POST.get('submit')
+    context = {
+        'username': username,
+        'password': password,
+        options: options,
+        submitbutton: 'Submit',
+    }
+    return render(request, 'UserInfo/')
---
+++ .history/Doctor_webApplication/main/views_20221027223036.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from djangp.
+from django.template import loader
+from .models import Hero
+def index(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def login(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
+
+def main(request):
+    hero = Hero()
+    hero.title = "Welcome Here!"
+    hero.sub_title = "We are so happy to be in your service"
+    hero.description = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum fugit est omnis ut cum vel libero accusantium necessitatibus placeat, assumenda consequuntur dicta tempore iusto et"
+    return render(request, 'main.html', {'hero': hero})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect('main:homepage')
