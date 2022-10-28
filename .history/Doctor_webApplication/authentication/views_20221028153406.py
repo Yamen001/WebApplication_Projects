37700c1e9ff74529e9a548cb6224cd1a2f6df4da
Kareem <kareemsafar2018@gmail.com>
2022-10-28 15:35:26 +0200

    configuring views -- not done --
    Signed-off-by: Kareem <kareemsafar2018@gmail.com>

---
+++ .history/Doctor_webApplication/authentication/views_20221028153406.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib 
+# Create your views here.
---
+++ .history/Doctor_webApplication/authentication/views_20221028153421.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.mail
+# Create your views here.
