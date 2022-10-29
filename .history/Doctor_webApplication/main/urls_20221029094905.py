2bd5531281a2ecfee8d0dbaab2f39671b8fa312f
Kareem <kareemsafar2018@gmail.com>
2022-10-29 10:37:39 +0200

    backend debugging
    Signed-off-by: Kareem <kareemsafar2018@gmail.com>

---
+++ .history/Doctor_webApplication/main/urls_20221029094905.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('index/', views.index, name = 'index'),
+    path('', views.main, name = 'main'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/',include('home.urls')),
+]
