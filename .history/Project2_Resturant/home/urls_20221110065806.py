fc1f4902fd2f91a4de89f107d7fc66df75c3d483
Kareem <kareemsafar2018@gmail.com>
2022-11-10 08:00:28 +0300

    backend part1
    Signed-off-by: Kareem <kareemsafar2018@gmail.com>

--- .history/Project2_Resturant/home/urls_20221110065806.py
+++ .history/Project2_Resturant/home/urls_20221110065806.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('', include('users.urls')),
 
     path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
-                                           authentication_form=LoginForm), name='login'),
+                                        authentication_form=LoginForm), name='login'),
 
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
