263103a4e1931f61804caa42941d2a1e1370de45
yamen <hzmkiller@gmail.com>
2022-11-17 21:02:33 +0300

    nav and landing preparing 60%

---
+++ Project2_Resturant/static/js/index.js
@@ -0,0 +1,19 @@
+// let landingfood = document.querySelector(".landing img")
+// let i =1
+// setInterval(function(){
+//     if(i === 1){
+//         landingfood.src = `/static/imgs/landingfood${i}.png`
+//         landingfood.style.animation = ""
+//         i++
+//     }
+//     else if(i ===2){
+//         landingfood.src = `/static/imgs/landingfood${i}.png`
+//         landingfood.style.animation = ""
+//         i++
+//     }
+//     else{
+//         landingfood.src = `/static/imgs/landingfood${i}.png`
+//         landingfood.style.animation = "fading 2s"
+//         i = 1
+//     }
+// },5000)
