1e0290b4c1efa1f03422d07570abd9fa65dda924
yamen <hzmkiller@gmail.com>
2022-11-18 19:30:13 +0300

    input page is done by 80%

--- Project2_Resturant/static/js/index.js
+++ Project2_Resturant/static/js/index.js
@@ -1,4 +1,18 @@
-// let landingfood = document.querySelector(".landing img")
+var lastScrollTop;
+navbar = document.querySelector('.navbar');
+console.log(getComputedStyle(navbar).height)
+window.addEventListener('scroll',function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){
+        var navbarHight = Number.parseInt(getComputedStyle(navbar).height)
+        navbar.style.top= -Number(navbarHight+10)+'px'
+    }
+    else{
+    navbar.style.top='0';
+    }
+    lastScrollTop = scrollTop;
+});
+//     let landingfood = document.querySelector(".landing img")
 // let i =1
 // setInterval(function(){
 //     if(i === 1){
@@ -17,3 +31,16 @@
 //         i = 1
 //     }
 // },5000)
+
+// const animationTime = 5000;
+// const transitionTime = 500;
+// function nextImage() {
+    
+//     let img = document.querySelector(".landing img")
+//     img.classList.remove('hidden');
+//     setTimeout(function () {
+//         img.classList.add('hidden');
+//     },animationTime-transitionTime);
+//     img.src=randomize();
+//     setTimeout(nextImage, animationTime);
+// }
