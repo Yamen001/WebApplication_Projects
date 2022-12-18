2d1e92ed682d2a0634ebc4caac97fcba6c744804
yamen <hzmkiller@gmail.com>
2022-12-18 11:12:08 +0300

    ordre page preparing

--- Project2_Resturant/static/js/order.js
+++ Project2_Resturant/static/js/order.js
@@ -1,31 +1,46 @@
 let list_toggler = document.querySelectorAll(".menuSction .list-toggler div")
-let Lists = document.querySelectorAll(".menuSction .lists div.list")
-console.log(Lists)
-
-list_toggler.forEach(element => {
-    document.addEventListener("click",function(e){
-        if(e.target.classList.contains("sec")){
+let Lists = document.querySelectorAll(".menuSction .lists .list")
+let Choosnlist
+let lists = ["breakfast","lunch","dinner","dessert"]
+document.addEventListener("click",function(e){
+    if(e.target.classList.contains("sec")){
+        list_toggler.forEach((element) => {
             if(element.classList.contains("checked")){
                 element.classList.remove("checked")
-            }
-            e.target.classList.add("checked")
-            // console.log(e.target.classList[1])
-            PutList(e.target.classList[1])
-        }
-        else{
-            console.log("no")
-        }
-    })
-});
+            }  
+        })
+        e.target.classList.add("checked")
+        Choosnlist = lists.indexOf(e.target.classList[1])
+        PutList(Choosnlist)
+    }
+})
 function PutList(list){
-    console.log(list)
-    Lists.forEach(function(e){
-        if(e.classList.contains(list)){
-            e.classList.add("addList")
-            console.log("hre")
+    Lists.forEach((e,i)=>{
+        if(i===list){
+            e.classList.add('d-flex')
+            e.classList.remove('d-none')
         }
-        else{
-            e.classList.remove("addList")
-        }
+        else(
+            e.classList.add("d-none")
+        )
     })
 }
+
+let OrderNow = document.querySelector("#orderNow")
+let OrderMenu = document.querySelector(".orderMenu")
+OrderNow.onclick= function(){
+    if(OrderMenu.classList.contains("closeMenu")){
+        OrderMenu.classList.remove("closeMenu")
+        OrderMenu.classList.add("openMenu")
+    }
+    else{
+        OrderMenu.classList.add("closeMenu")
+    }
+}
+
+
+// lists imgs fix
+// ordre menu imgs fix
+// open and close buttons fix
+// add intro section 
+// clean code
