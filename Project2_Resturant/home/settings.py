af3f6b34ae02d295db803f7c7bb634afa535c766
kareemIOI <kareemiisafar@gmail.com>
2023-02-27 08:44:18 -0800

    debugging resturant - not done -

--- Project2_Resturant/home/settings.py
+++ Project2_Resturant/home/settings.py
@@ -76,12 +76,9 @@ WSGI_APPLICATION = "home.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": 'Resturantdb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- Project2_Resturant/main/views.py
+++ Project2_Resturant/main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.conf import settings
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
